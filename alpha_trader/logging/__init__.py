7ceb54e93716a4df73f1d7ac091dd480edd51157
maltemelzer <maltemelzer@gmx.de>
2023-05-01 16:29:30 +0200

    fix(logging): remove logging to file

--- alpha_trader/logging/__init__.py
+++ alpha_trader/logging/__init__.py
@@ -5,17 +5,13 @@ logger.setLevel(logging.DEBUG)
 
 # create console handler and set level to debug
 ch = logging.StreamHandler()
-file_handler = logging.FileHandler("logs/example.log")
 ch.setLevel(logging.DEBUG)
-file_handler.setLevel(logging.DEBUG)
 
 # create formatter
 formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 
 # add formatter to ch
 ch.setFormatter(formatter)
-file_handler.setFormatter(formatter)
 
 # add ch to logger
 logger.addHandler(ch)
-logger.addHandler(file_handler)
